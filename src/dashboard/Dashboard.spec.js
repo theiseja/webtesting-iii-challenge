3d7af5bf28b0e1dfe470d9582c9cd4084ec47efb
Jesse Theisen <theiseja@gmail.com>
2019-04-17 23:16:56 -0500

    MVP: Started writing dashboard tests

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,19 @@
+import React from 'react';
+import { render } from 'react-testing-library';
+import 'react-testing-library/cleanup-after-each';
+import Dashboard from './dashboard/dashboard';
+
+describe('Dashboard component tests', () => {
+    test('should render controls and display', () => {
+        const { getByText } = render(<Dashboard />);
+
+        getByText(/unlocked/i);
+        getByText(/open/i);
+        getByText(/lock gate/i);
+        getByText(/close gate/i);
+    });
+});
+
+
+
 // Test away
