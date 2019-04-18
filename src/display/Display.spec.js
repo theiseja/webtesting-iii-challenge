629b848b4c88d142622cb44854d4ec555b562298
Jesse Theisen <theiseja@gmail.com>
2019-04-18 00:09:05 -0500

    MVP: Wrote Display test to show if gate is unlocked and open

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,22 @@
+import React from 'react';
+import { toHaveClass } from 'jest-dom';
+import { render, fireEvent, cleanup } from 'react-testing-library';
+import 'react-testing-library/cleanup-after-each';
+
+import Display from './display/display';
+
+expect.extend({ toHaveClass });
+
+describe('Display component tests', () => {
+    it('should display component', () => {
+        render(<Display />);
+    });
+
+    it('should display if gate is unlocked and open by default', () => {
+        const { getByText } = render(<Display />);
+
+        getByText(/unlocked/i);
+        getByText(/open/i);
+    });
+});
 // Test away!
