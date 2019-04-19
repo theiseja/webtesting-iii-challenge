e05379b443dc67e752b621e7c301842a23ff420a
Jesse Theisen <theiseja@gmail.com>
2019-04-18 20:20:56 -0500

    MVP: Wrote Control test that renders button to toggle closed and locked states

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,19 @@
+import React from 'react';
+import { toBeDisabled } from 'jest-dom';
+import {render, fireEvent } from 'react-testing-library';
+import 'react-testing-library/cleanup-after-each';
+
+import Controls from '../controls/Controls';
+
+expect.extend({ toBeDisabled });
+
+describe('Control component tests', () => {
+    it('should render button to toggle closed and locked states', () => {
+        const { getByText } = render(<Controls />);
+
+        getByText(/close gate/i);
+        getByText(/lock gate/i);
+    });
+});
+
 // Test away!
